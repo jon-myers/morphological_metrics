df2442ae52bf12b06f645d1fc7a006ffa1292bcd
jon-myers <jon.myers.sound@gmail.com>
2023-08-15 17:55:44 -0400

    current

--- src/morphs.test.ts
+++ src/morphs.test.ts
@@ -1,15 +1,91 @@
-import { Morph } from './morphs';
+import { Morph, MorphologicalMetric, delta } from './morphs';
+
+const morphGen = (n: number) => {
+  return new Morph([...Array(n)].map(() => {
+    return Math.floor(12 * Math.random())
+  }))
+};
 
 describe('Morph', () => {
-  test('Creates a morph from 1d data input', () => {
+  test('Creates a morph', () => {
     const m = new Morph([1, 2, 3]);
-    expect(m.dimensions).toBe(1);
-    expect(m.data).toEqual([[1, 2, 3]])
+    expect(m.data).toEqual([1, 2, 3]);
+
   });
 
-  test('Creates a morph from 2d data input', () => {
-    const m = new Morph([[1, 2, 3], [4, 5, 6]]);
-    expect(m.dimensions).toBe(2);
-    expect(m.data).toEqual([[1, 2, 3], [4, 5, 6]])
-  });
+  test('Tries to create a morph with only one point', () => {
+    expect(() => new Morph([1])).toThrow();
+  })
+
+  test('Calculates Absolute Intervals, 1d', () => {
+    const m = new Morph([0, 2, 4, 1, 0]);
+    const n = new Morph([2, 3, 0, 4, 1]);
+    expect(m.firstOrderAbsoluteInterval).toEqual([2, 2, 3, 1]);
+    expect(n.firstOrderAbsoluteInterval).toEqual([1, 3, 4, 3]);
+    expect(m.secondOrderAbsoluteInterval).toEqual([0, 1, 2]);
+    expect(n.secondOrderAbsoluteInterval).toEqual([2, 1, 1]);
+  })
+
+  test('Calculates Derivatives', () => {
+    const m = new Morph([0, 2, 4, 1, 0]);
+    expect(m.derivate(1)).toEqual([2, 2, -3, -1]);
+    expect(m.derivate(2)).toEqual([0, -5, 2]);
+    expect(m.derivate(3)).toEqual([-5, 7]); 
+  })
+
+  test('Calculates Linear Contour Vectors, 1d', () => {
+    const m = new Morph([2, 3, 4, 5]);
+    expect(m.linearContourVector).toEqual([3, 0, 0]);
+    const n = new Morph([5, 4, 3, 2]);
+    expect(n.linearContourVector).toEqual([0, 0, 3]);
+    const o = new Morph([5, 9, 3, 2]);
+    expect(o.linearContourVector).toEqual([1, 0, 2]);
+    const p = new Morph([2, 5, 6, 6]);
+    expect(p.linearContourVector).toEqual([2, 1, 0]);
+  })
+
+  test('Tries out Magnitude Metric', () => {
+    const m = new Morph([0, 2, 5, 2, 1]);
+    const n = new Morph([4, 4, 1, 6, 2]);
+    const mm = new MorphologicalMetric([m, n]);
+    expect(mm.MagnitudeMetric()).toEqual(7/4);
+    expect(mm.MagnitudeMetric({ normalized: false })).toEqual(7);
+    expect(mm.MagnitudeMetric({ absolute: false })).toEqual(3/4);
+  })
+
+  test('Sobalev OLM', () => {
+    const m = new Morph([0, 2, 5, 2, 1]);
+    const n = new Morph([4, 4, 1, 6, 2]);
+    const mm = new MorphologicalMetric([m, n]);
+    const ans = (3 + 7/8 + 5/9) / (1 + 1/2 + 1/3);
+    expect(mm.SobalevOLM({ weights: [1, 1/2, 1/3] })).toEqual(ans)
+    const ans2 = (7/8 + 5/9) / (1/2 + 1/3);
+    expect(mm.SobalevOLM({ minOrder: 1, weights: [1/2, 1/3] })).toEqual(ans2)
+  })
+
+  test('Tries out OLM', () => {
+    const m = new Morph([0, 2, 4, 1, 0]);
+    const n = new Morph([2, 3, 0, 4, 1]);
+    const mm = new MorphologicalMetric([m, n]);
+    expect(mm.OLM({ squared: false, order: 1 })).toEqual(1.25);
+    expect(mm.OLM({ squared: false, order: 2 })).toEqual(1);
+    const ans = (3 ** 0.5 + 5 ** 0.5 + 7 ** 0.5 + 8 ** 0.5) / 4;
+    expect(mm.OLM({ squared: true, order: 1 })).toEqual(ans); // ~2.360
+    const ans2 = (4 ** 0.5 + 3 ** 0.5) / 3;
+    expect(mm.OLM({ squared: true, order: 2 })).toEqual(ans2); // ~1.244
+  })
+
+  test('OLMGeneral', () => {
+    const m = morphGen(10);
+    const n = morphGen(10);
+    const mm = new MorphologicalMetric([m, n]);
+    expect(mm.OLM({ squared: false, order: 1 })).toEqual(mm.OLMGeneral());
+  })
+
+  test('OLMGeneral interval class', () => {
+    const m = new Morph([0, 2, 5, 4, 1]);
+    const n = new Morph([4, 5, 4, 3, 2]);
+    const mm = new MorphologicalMetric([m, n]);
+  })
+
 })
--- src/morphs.ts
+++ src/morphs.ts
@@ -4,19 +4,231 @@ const isNestedArray = (data: number[][] | number[]): boolean => {
   return Array.isArray(data[0]);
 }
 
-class Morph {
-  dimensions: number;
-  data: number[][];
+const mod = (n: number, m: number) => {
+  return ((n % m) + m) % m;
+}
 
-  constructor(data: number[][] | number[]) {
-    if (isNestedArray(data)) {
-      this.dimensions = data.length;
-      this.data = data as number[][];
-    } else {
-      this.dimensions = 1;
-      this.data = [data as number[]];
+type LinearContourVector = [number, number, number];
+
+class Morph {
+  data: number[];
+
+  constructor(data: number[]) {
+
+    this.data = data;
+    
+
+    if (this.data.length < 2) {
+      throw new Error('Arrays must have at least 2 elements');
     }
+  }
+
+  derivate(order=1, absolute=false) { // gets the nth order difference of the data
+    let out: number[];
+    if (this.data.length - order < 1) {
+      throw new Error('Order must be less than the length of the array');
+    }
+    if (order < 0) {
+      throw new Error('Order must be greater than 0');
+    } else if (order === 0) {
+      out = this.data;
+    } else if (order === 1) {
+      out = this.data.slice(1).map((_, i) => {
+        return this.data[i + 1] - this.data[i];
+      })
+    } else {
+      const data: number[] = this.derivate(order - 1, absolute);
+      out = data.slice(1).map((_, i) => {
+        return data[i + 1] - data[i];
+      })
+    }
+    if (absolute) {
+      out = out.map(n => Math.abs(n));
+    }
+    return out
+  }
+
+  get firstOrderAbsoluteInterval() {
+    return this.derivate(1, true)
+  }
+
+  get secondOrderAbsoluteInterval() {
+    return this.derivate(2, true)
+  }
+
+  get linearContourVector() {
+    const lcv: LinearContourVector = [0, 0, 0];
+    this.derivate(1).forEach(ai => {
+      if (ai > 0) {
+        lcv[0] += 1;
+      } else if (ai === 0) {
+        lcv[1] += 1;
+      } else {
+        lcv[2] += 1;
+      }
+    })
+    return lcv;
   }
 }
 
-export { Morph }
+class MorphologicalMetric {
+  morphs: [Morph, Morph];
+  ordered: boolean;
+
+  constructor(morphs: [Morph, Morph], ordered: boolean = true) {
+    if (ordered && morphs[0].data.length !== morphs[1].data.length) {
+      throw new Error('Ordered mms must have the same number of points');
+    }
+
+    this.morphs = morphs;
+    this.ordered = ordered;
+  };
+
+  // Polansky, 1996, pg. 300 - 301
+  // Note: LP's equation as written out, for the squared form of OLM is 
+  // incorrect. The `(m_i - m_i+1)^2 - (n_i - n_i+1)^2` must be wrapped in
+  // an absolute value function, otherwise the result is sometimes imaginary.
+  // Also, the example of the OLM sqaured form (pg 301), 2nd order is incorrect: 
+  // it should be `OLM, no delta^2 (2nd order) = (4 ^ 0.5 + 3 ^ 0.5) / 3 = ~1.244`
+  OLM({
+    squared = false,
+    order = 1
+  }: {
+    squared?: boolean,
+    order?: number
+  } = {}) { // ordered linear magnitude
+    if (!this.ordered) {
+      throw new Error('Morphs must be ordered for OLM');
+    }
+    if (order < 1) {
+      throw new Error('Order must be greater than 0');
+    }
+    const [m, n] = this.morphs;
+    const diff = (m: number, n: number) => {
+      if (squared) {
+        return Math.abs(m ** 2 - n ** 2) ** 0.5;
+      } else {
+        return Math.abs(Math.abs(m) - Math.abs(n));
+      }
+    };
+    const out = m.derivate(order, true).map((mDelta, i) => {
+      const nDelta = n.derivate(order, true)[i];
+      const difference = diff(mDelta, nDelta);
+      return difference
+    }).reduce((a, b) => a + b, 0);
+    return out / (m.data.length - order);
+  }
+
+  // Polansky, 1996, pg. 300
+  SobalevOLM({
+    maxOrder = 2, // inclusive
+    minOrder = 0,
+    weights = undefined
+  }: {
+    maxOrder?: number,
+    minOrder?: number,
+    weights?: number[]
+  } = {}) {
+    if (!this.ordered) {
+      throw new Error('Morphs must be ordered for SobalevOLM');
+    }
+    if (maxOrder - minOrder < 1) { 
+      throw new Error('Max order must be greater than min order');
+    }
+    if (weights === undefined) {
+      weights = Array(maxOrder - minOrder).fill(1) as number[];
+    } else if (weights.length !== maxOrder - minOrder + 1) {
+      throw new Error('Weights must be the same length as the number of orders');
+    }
+    const [m, n] = this.morphs;
+    const normedSums = [];
+    for (let i = minOrder; i <= maxOrder; i++) {
+      const mDeriv = m.derivate(i, true);
+      const nDeriv = n.derivate(i, true);
+      let sum = 0;
+      for (let j = 0; j < mDeriv.length; j++) {
+        sum += Math.abs(mDeriv[j] - nDeriv[j]);
+      }
+      normedSums.push(sum / mDeriv.length);
+    }
+    const weightedSums = normedSums.map((sum, i) => sum * weights![i]);
+    const summedWeights = weights.reduce((a, b) => a + b, 0);
+    return weightedSums.reduce((a, b) => a + b, 0) / summedWeights;
+  }
+
+  // Polansky, 1996, pg. 299
+  MagnitudeMetric({
+    absolute = true, 
+    normalized = true
+  }: {
+    absolute?: boolean,
+    normalized?: boolean
+  } = {}) { 
+    const [m, n] = this.morphs;
+    const mDeriv = m.derivate(1, absolute);
+    const nDeriv = n.derivate(1, absolute);
+    let out = mDeriv.map((_, i) => {
+      if (absolute) {
+        return Math.abs(mDeriv[i] - nDeriv[i]);
+      } else {
+        return mDeriv[i] - nDeriv[i];
+      }
+    }).reduce((a, b) => a + b, 0);
+    if (normalized) {
+      out /= mDeriv.length;
+    }
+    return out
+  }
+
+  // Polansky, 1996, pg. 302
+  OLMGeneral({
+    delta = (a: number, b: number) => Math.abs(a - b),
+    order = 1
+  }: {
+    delta?: (a: number | PitchClassType, b: number | PitchClassType) => number,
+    order?: number
+  } = {}) {
+    const [m, n] = this.morphs;
+    const mDeriv = m.derivate(order, true);
+    const nDeriv = n.derivate(order, true);
+    let out = mDeriv.map((_, i) => {
+      return delta(mDeriv[i], nDeriv[i]);
+    }).reduce((a, b) => a + b, 0);
+    return out / mDeriv.length;
+  }
+}
+
+type PitchClassType = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+type IntervalClassType = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+const delta = {
+  intervalClass: (a: PitchClassType, b: PitchClassType): IntervalClassType => {
+    return Math.min(mod(a - b, 12), mod(b - a, 12)) as IntervalClassType;
+  }
+}
+
+
+
+
+// class Delta {
+//   comparands: [number, number];
+//   type: (
+//     'absolute value' | 
+//     'ratio' | 
+//     'max of function' | 
+//     'squared difference' | 
+//     'root of squared difference'
+//   );
+// }
+
+// const m = new Morph([0, 2, 4, 1, 0]);
+// const n = new Morph([2, 3, 0, 4, 1]);
+
+// const ai = m.firstOrderAbsoluteInterval;
+// console.log(ai)
+
+
+
+
+
+export { Morph, MorphologicalMetric, delta }
